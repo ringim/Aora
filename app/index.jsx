02fd2ba3e74cdecf4e40873f6961e495d120a914
Abubakar Kabiru <abubakarringim@gmail.com>
2024-07-27 12:58:06 +0100

    Implemented Routing Links BTW Screens

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aora!</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{color:'blue'}}>
+        <Text>Go to Profile</Text>
+      </Link>
     </View>
   );
 }
